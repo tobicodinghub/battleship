53b615f4505eefd2e55f1245e0c347828a2f28de
Tobias Hornbogen <hornbogent@helmholtzschule.de>
2022-12-10 19:02:10 +0100

    pictures and first gui commit

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import random
 from tkinter import *
+import tkinter as tk
 common_ships = {
     "carrier": 5,
     "battleship": 4,
@@ -8,10 +9,63 @@ common_ships = {
     "destroyer": 2
 }
 
+
+class Window():
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Battleship")
+        self.window.geometry("1920x1080")
+        self.bgimg = tk.PhotoImage(file="battleship.png")
+        self.limg = Label(self.window, image=self.bgimg)
+        self.limg.pack()
+
+    def initialize_matrix(self, matrix, xc, yc):
+        btns_frame = Frame(self.window)
+        btns_frame.pack()
+        water = tk.PhotoImage(file="crosscircle.png")
+        for row in range(len(matrix)):
+            for col in range(len(matrix[row])):
+                i = matrix[row][col]
+                b = Button(btns_frame, text=str(i), height=3, width=5, padx="5px",
+                           pady="5px", activeforeground="blue", activebackground="red")
+                b.grid(row=row + 1, column=col)
+        btns_frame.place(x=xc, y=yc)
+
+    def place_ships(self, matrix, ships):
+        i = 0
+        x = []
+        y = []
+        z = []
+        for ship in ships:
+            Label(self.window, text=ship, font=("Arial", 40)).place(x=1200, y=150 + 150*i)
+            x.append(Entry(self.window).place(x=1200, y=250 + 150*i))
+            y.append(Entry(self.window).place(x=1350, y=250 + 150*i))
+            z.append(Entry(self.window).place(x=1500, y=250 + 150*i))
+            i += 1
+        
+        for ship in ships:
+            i = 0
+            a = x[i].get()
+            b = y[i].get()
+            dir = z[i].get()
+            if dir == "h":
+                    for i in range(self.ships[ship]):
+                        matrix[a][b+i] = 1
+            else:
+                for i in range(self.ships[ship]):
+                    self.matrix[a+i][b] = 1    
+            i += 1
+
 class Game():
-    def __init__(self, player1, computer):
+    def __init__(self, window, player1, computer):
+        self.window = window
         self.player1 = player1
         self.computer = computer
+        self.window.initialize_matrix(self.player1.sea.matrix, 170, 150)
+        self.window.place_ships(self.player1.sea.matrix,
+                                self.player1.sea.ships)
+        #self.window.initialize_matrix(self.computer.sea.matrix, 1175, 150)
+
         self.turn = 1
         self.winner = None
         self.play_game()
@@ -44,7 +98,6 @@ class Game():
 class Player():
     def __init__(self, sea):
         self.sea = sea
-        self.sea.place_ships()
         self.tiles_sunk = 0
         self.visible_matrix = [[0 for x in range(10)] for y in range(10)]
 
@@ -98,30 +151,6 @@ class Sea:
             print(" ".join(str(cell) for cell in row))
         print('\n')
 
-    def place_ships(self):
-        for ship in self.ships:
-            print(ship)
-            x = int(input("x: "))
-            y = int(input("y: "))
-            direction = input("direction: ")
-            if direction == "h":
-                while not self.check_out_of_bounds(x, y, direction, ship) or not self.has_collision(x, y, direction, ship, self.matrix):
-                    print("Invalid placement!")
-                    x = int(input("x: "))
-                    y = int(input("y: "))
-                for i in range(self.ships[ship]):
-                    self.matrix[x][y+i] = 1
-            elif input == "v":
-                while not self.check_out_of_bounds(x, y, direction, ship) or not self.has_collision(x, y, direction, ship, self.matrix):
-                    print("Invalid placement!")
-                    x = int(input("x: "))
-                    y = int(input("y: "))
-                    direction = input("direction: ")
-                for i in range(self.ships[ship]):
-                    self.matrix[x+i][y] = 1
-            self.print_matrix()
-        self.print_matrix()
-
     def check_out_of_bounds(self, x, y, dir, ship):
         if dir == "h":
             return y + self.ships[ship] <= 9
@@ -154,7 +183,6 @@ class Sea:
             else:
                 for i in range(self.ships[ship]):
                     self.matrix[x+i][y] = 1
-        self.print_matrix()
 
 
-game1 = Game(Player(Sea(common_ships)), Computer(Sea(common_ships)))
+game1 = Game(Window(), Player(Sea(common_ships)), Computer(Sea(common_ships)))
