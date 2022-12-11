57043425ea7b78fda1bb00132cd4e29508bd6bd1
Tobias Hornbogen <hornbogent@helmholtzschule.de>
2022-12-11 12:48:01 +0100

    gui changes

--- game.py
+++ game.py
@@ -32,36 +32,99 @@ class Window():
         btns_frame.place(x=xc, y=yc)
 
     def place_ships(self, matrix, ships):
+        a = []
+        b = []
+        c = []
         i = 0
-        x = []
-        y = []
-        z = []
+        x1 = tk.StringVar(self.window)
+        x2 = tk.StringVar(self.window)
+        x3 = tk.StringVar(self.window)
+        x4 = tk.StringVar(self.window)
+        x5 = tk.StringVar(self.window)
+        y1 = tk.StringVar(self.window)
+        y2 = tk.StringVar(self.window)
+        y3 = tk.StringVar(self.window)
+        y4 = tk.StringVar(self.window)
+        y5 = tk.StringVar(self.window)
+        dir1 = tk.StringVar(self.window)
+        dir2 = tk.StringVar(self.window)
+        dir3 = tk.StringVar(self.window)
+        dir4 = tk.StringVar(self.window)
+        dir5 = tk.StringVar(self.window)
         for ship in ships:
-            Label(self.window, text=ship, font=("Arial", 40)).place(x=1200, y=150 + 150*i)
-            x.append(Entry(self.window).place(x=1200, y=250 + 150*i))
-            y.append(Entry(self.window).place(x=1350, y=250 + 150*i))
-            z.append(Entry(self.window).place(x=1500, y=250 + 150*i))
-            i += 1
-        
-        for ship in ships:
-            i = 0
-            a = x[i].get()
-            b = y[i].get()
-            dir = z[i].get()
-            if dir == "h":
-                    for i in range(self.ships[ship]):
-                        matrix[a][b+i] = 1
-            else:
-                for i in range(self.ships[ship]):
-                    self.matrix[a+i][b] = 1    
-            i += 1
+            Label(self.window, text=ship, font=(
+                "Arial", 40)).place(x=1200, y=150 + 150*i).pack()
 
+            i += 1
+        Entry(self.window, textvariable=x1).place(x=1200, y=250)
+        Entry(self.window, textvariable=y1).place(x=1350, y=250)
+        Entry(self.window, textvariable=dir1).place(x=1500, y=250)
+        button1 = Button(self.window, text="Place Ship", command=self.get_ship(
+            x1, y1, dir1,a,b,c)).place(x=1650, y=250)
+        Entry(self.window, textvariable=x2).place(x=1200, y=400)
+        Entry(self.window, textvariable=y2).place(x=1350, y=400)
+        Entry(self.window, textvariable=dir2).place(x=1500, y=400)
+        button2 = Button(self.window, text="Place Ship", command=self.get_ship(
+            x2, y2, dir2,a,b,c)).place(x=1650, y=400)
+        Entry(self.window, textvariable=x3).place(x=1200, y=550)
+        Entry(self.window, textvariable=y3).place(x=1350, y=550)
+        Entry(self.window, textvariable=dir3).place(x=1500, y=550)
+        button3 = Button(self.window, text="Place Ship", command=self.get_ship(
+            x3, y3, dir3,a,b,c)).place(x=1650, y=550)
+        Entry(self.window, textvariable=x4).place(x=1200, y=700)
+        Entry(self.window, textvariable=y4).place(x=1350, y=700)
+        Entry(self.window, textvariable=dir4).place(x=1500, y=700)
+        button4 = Button(self.window, text="Place Ship", command=self.get_ship(
+            x4, y4, dir4,a,b,c)).place(x=1650, y=700)
+        Entry(self.window, textvariable=x5).place(x=1200, y=850)
+        Entry(self.window, textvariable=y5).place(x=1350, y=850)
+        Entry(self.window, textvariable=dir5).place(x=1500, y=850)
+        button5 = Button(self.window, text="Place Ship", command=self.get_last_ship(
+            x5, y5, dir5,a,b,c, matrix, ships)).place(x=1650, y=850)
+
+    def get_ship(self, x, y, dir, a, b, c):
+        a.append(x.get())
+        b.append(y.get())
+        c.append(dir.get())
+    def get_last_ship(self, x, y, dir, a, b, c, matrix, ships):
+            a.append(x.get())
+            b.append(y.get())
+            c.append(dir.get())   
+            self.place_ship(matrix, ships, a, b, c)    
+
+    def place_ship(self, matrix, ships, a, b, c):
+        for ship in ships:
+            if ship == "carrier":
+                x = a[0]
+                y = b[0]
+                dir = c[0]
+            elif ship == "battleship":
+                x = a[1]
+                y = b[1]
+                dir = c[1]
+            elif ship == "cruiser":
+                x = a[2]
+                y = b[2]
+                dir = c[2]
+            elif ship == "submarine":
+                x = a[3]
+                y = b[3]
+                dir = c[3]
+            elif ship == "destroyer":
+                x = a[4]
+                y = b[4]
+                dir = c[4]
+            if dir == "h":
+                for i in range(ships[ship]):
+                    matrix[int(x)][int(y) + i] = ship[0]
+            else:
+                for i in range(ships[ship]):
+                    matrix[int(x) + i][int(y)] = ship[0]
 class Game():
     def __init__(self, window, player1, computer):
         self.window = window
         self.player1 = player1
         self.computer = computer
-        self.window.initialize_matrix(self.player1.sea.matrix, 170, 150)
         self.window.place_ships(self.player1.sea.matrix,
                                 self.player1.sea.ships)
         #self.window.initialize_matrix(self.computer.sea.matrix, 1175, 150)
