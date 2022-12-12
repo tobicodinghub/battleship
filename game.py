20112aeb76d08dc21cc8e7fe85eb3422a0e358c0
Tobias Hornbogen <hornbogent@helmholtzschule.de>
2022-12-12 11:48:16 +0100

    JAAAAAAAAAAAAAAAAAA

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import random
 from tkinter import *
+from functools import partial
 import tkinter as tk
 common_ships = {
     "carrier": 5,
@@ -10,17 +11,17 @@ common_ships = {
 }
 
 
-class Window():
+class Window(tk.Tk):
     def __init__(self):
-        self.window = tk.Tk()
-        self.window.title("Battleship")
-        self.window.geometry("1920x1080")
+        super().__init__()
+        self.title("Battleship")
+        self.geometry("1920x1080")
         self.bgimg = tk.PhotoImage(file="battleship.png")
-        self.limg = Label(self.window, image=self.bgimg)
+        self.limg = Label(self, image=self.bgimg)
         self.limg.pack()
 
     def initialize_matrix(self, matrix, xc, yc):
-        btns_frame = Frame(self.window)
+        btns_frame = Frame(self)
         btns_frame.pack()
         water = tk.PhotoImage(file="crosscircle.png")
         for row in range(len(matrix)):
@@ -36,61 +37,62 @@ class Window():
         b = []
         c = []
         i = 0
-        x1 = tk.StringVar(self.window)
-        x2 = tk.StringVar(self.window)
-        x3 = tk.StringVar(self.window)
-        x4 = tk.StringVar(self.window)
-        x5 = tk.StringVar(self.window)
-        y1 = tk.StringVar(self.window)
-        y2 = tk.StringVar(self.window)
-        y3 = tk.StringVar(self.window)
-        y4 = tk.StringVar(self.window)
-        y5 = tk.StringVar(self.window)
-        dir1 = tk.StringVar(self.window)
-        dir2 = tk.StringVar(self.window)
-        dir3 = tk.StringVar(self.window)
-        dir4 = tk.StringVar(self.window)
-        dir5 = tk.StringVar(self.window)
+        x1 = tk.StringVar(self)
+        x2 = tk.StringVar(self)
+        x3 = tk.StringVar(self)
+        x4 = tk.StringVar(self)
+        x5 = tk.StringVar(self)
+        y1 = tk.StringVar(self)
+        y2 = tk.StringVar(self)
+        y3 = tk.StringVar(self)
+        y4 = tk.StringVar(self)
+        y5 = tk.StringVar(self)
+        dir1 = tk.StringVar(self)
+        dir2 = tk.StringVar(self)
+        dir3 = tk.StringVar(self)
+        dir4 = tk.StringVar(self)
+        dir5 = tk.StringVar(self)
         for ship in ships:
-            Label(self.window, text=ship, font=(
-                "Arial", 40)).place(x=1200, y=150 + 150*i).pack()
-
+            Label(self, text=ship, font=(
+                "Arial", 40)).place(x=1200, y=150 + 150*i)
             i += 1
-        Entry(self.window, textvariable=x1).place(x=1200, y=250)
-        Entry(self.window, textvariable=y1).place(x=1350, y=250)
-        Entry(self.window, textvariable=dir1).place(x=1500, y=250)
-        button1 = Button(self.window, text="Place Ship", command=self.get_ship(
-            x1, y1, dir1,a,b,c)).place(x=1650, y=250)
-        Entry(self.window, textvariable=x2).place(x=1200, y=400)
-        Entry(self.window, textvariable=y2).place(x=1350, y=400)
-        Entry(self.window, textvariable=dir2).place(x=1500, y=400)
-        button2 = Button(self.window, text="Place Ship", command=self.get_ship(
-            x2, y2, dir2,a,b,c)).place(x=1650, y=400)
-        Entry(self.window, textvariable=x3).place(x=1200, y=550)
-        Entry(self.window, textvariable=y3).place(x=1350, y=550)
-        Entry(self.window, textvariable=dir3).place(x=1500, y=550)
-        button3 = Button(self.window, text="Place Ship", command=self.get_ship(
-            x3, y3, dir3,a,b,c)).place(x=1650, y=550)
-        Entry(self.window, textvariable=x4).place(x=1200, y=700)
-        Entry(self.window, textvariable=y4).place(x=1350, y=700)
-        Entry(self.window, textvariable=dir4).place(x=1500, y=700)
-        button4 = Button(self.window, text="Place Ship", command=self.get_ship(
-            x4, y4, dir4,a,b,c)).place(x=1650, y=700)
-        Entry(self.window, textvariable=x5).place(x=1200, y=850)
-        Entry(self.window, textvariable=y5).place(x=1350, y=850)
-        Entry(self.window, textvariable=dir5).place(x=1500, y=850)
-        button5 = Button(self.window, text="Place Ship", command=self.get_last_ship(
-            x5, y5, dir5,a,b,c, matrix, ships)).place(x=1650, y=850)
+        Entry(self, textvariable=x1).place(x=1200, y=250)
+        Entry(self, textvariable=y1).place(x=1350, y=250)
+        Entry(self, textvariable=dir1).place(x=1500, y=250)
+        button1 = Button(self, text="Place Ship", command=partial(
+            self.get_ship, x1, y1, dir1, a, b, c)).place(x=1650, y=250)
+        Entry(self, textvariable=x2).place(x=1200, y=400)
+        Entry(self, textvariable=y2).place(x=1350, y=400)
+        Entry(self, textvariable=dir2).place(x=1500, y=400)
+        button2 = Button(self, text="Place Ship", command=partial(
+            self.get_ship, x2, y2, dir2, a, b, c)).place(x=1650, y=400)
+        Entry(self, textvariable=x3).place(x=1200, y=550)
+        Entry(self, textvariable=y3).place(x=1350, y=550)
+        Entry(self, textvariable=dir3).place(x=1500, y=550)
+        button3 = Button(self, text="Place Ship", command=partial(
+            self.get_ship, x3, y3, dir3, a, b, c)).place(x=1650, y=550)
+        Entry(self, textvariable=x4).place(x=1200, y=700)
+        Entry(self, textvariable=y4).place(x=1350, y=700)
+        Entry(self, textvariable=dir4).place(x=1500, y=700)
+        button4 = Button(self, text="Place Ship", command=partial(
+            self.get_ship, x4, y4, dir4, a, b, c)).place(x=1650, y=700)
+        Entry(self, textvariable=x5).place(x=1200, y=850)
+        Entry(self, textvariable=y5).place(x=1350, y=850)
+        Entry(self, textvariable=dir5).place(x=1500, y=850)
+        button5 = Button(self, text="Place Ship", command=partial(
+            self.get_last_ship, x5, y5, dir5, a, b, c, matrix, ships)).place(x=1650, y=850)
 
     def get_ship(self, x, y, dir, a, b, c):
         a.append(x.get())
         b.append(y.get())
         c.append(dir.get())
+
     def get_last_ship(self, x, y, dir, a, b, c, matrix, ships):
-            a.append(x.get())
-            b.append(y.get())
-            c.append(dir.get())   
-            self.place_ship(matrix, ships, a, b, c)    
+        a.append(x.get())
+        b.append(y.get())
+        c.append(dir.get())
+        self.place_ship(matrix, ships, a, b, c)
+        self.initialize_matrix(matrix, 150, 150)
 
     def place_ship(self, matrix, ships, a, b, c):
         for ship in ships:
@@ -116,24 +118,27 @@ class Window():
                 dir = c[4]
             if dir == "h":
                 for i in range(ships[ship]):
-                    matrix[int(x)][int(y) + i] = ship[0]
+                    matrix[int(x)][int(y) + i] = 1
             else:
                 for i in range(ships[ship]):
-                    matrix[int(x) + i][int(y)] = ship[0]
+                    matrix[int(x) + i][int(y)] = 1
+
+
 class Game():
-    def __init__(self, window, player1, computer):
-        self.window = window
+    def __init__(self, player1, computer):
+        self.window = Window()
         self.player1 = player1
         self.computer = computer
-        self.window.place_ships(self.player1.sea.matrix,
-                                self.player1.sea.ships)
+        self.window.initialize_matrix(self.player1.sea.matrix, 150, 150)
         #self.window.initialize_matrix(self.computer.sea.matrix, 1175, 150)
-
         self.turn = 1
         self.winner = None
         self.play_game()
+        self.window.mainloop()
 
     def play_game(self):
+        self.window.place_ships(self.player1.sea.matrix,
+                                self.player1.sea.ships)
         while not self.winner:
             self.take_turn()
             self.check_winner()
@@ -248,4 +253,4 @@ class Sea:
                     self.matrix[x+i][y] = 1
 
 
-game1 = Game(Window(), Player(Sea(common_ships)), Computer(Sea(common_ships)))
+game1 = Game(Player(Sea(common_ships)), Computer(Sea(common_ships)))
